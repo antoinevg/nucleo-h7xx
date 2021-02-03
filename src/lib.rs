76e277695aa973f0d9c9fc6834e3ccd38985571a
Antoine van Gelder <antoine@flowdsp.io>
2021-02-03 16:32:19 +0200

    Move ownership of ccdr.peripheral to Board after initialization

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,18 @@ pub mod usart;
 //#[macro_use]
 //pub mod itm_macros;
 use panic_semihosting as _;
-//use cortex_m_rt::entry;
+
+
+// - global static state ------------------------------------------------------
+
+// `no_mangle` is used here to prevent linking different minor
+// versions of this crate as that would let you `take` the core
+// peripherals more than once (one per minor version)
+#[no_mangle]
+static DAISY_BOARD: () = ();
+
+/// Set to `true` when `take` was called to make `Board` a singleton.
+static mut TAKEN: bool = false;
 
 
 // - Board --------------------------------------------------------------------
@@ -37,6 +48,7 @@ use panic_semihosting as _;
 #[allow(non_snake_case)]
 pub struct Board<'a> {
     pub clocks: hal::rcc::CoreClocks,
+    pub peripheral: hal::rcc::rec::PeripheralREC,
     pub pins: pin::Pins,
     pub leds: led::Leds,
 
@@ -44,16 +56,6 @@ pub struct Board<'a> {
 
     _marker: core::marker::PhantomData<&'a *const ()>,
 }
-
-
-// NOTE `no_mangle` is used here to prevent linking different minor versions of this crate as that
-// would let you `take` the core peripherals more than once (one per minor version)
-#[no_mangle]
-static NUCLEO_BOARD: () = ();
-
-/// Set to `true` when `take` or `steal` was called to make `Board` a singleton.
-static mut TAKEN: bool = false;
-
 
 impl<'a> Board<'a> {
     /// Returns the nucleo board *once*
@@ -70,16 +72,12 @@ impl<'a> Board<'a> {
         })
     }
 
-    /// Unchecked version of `Board::take`
-    #[inline]
-    pub unsafe fn steal() -> Self {
-        Self::new(pac::CorePeripherals::steal(),
-                  pac::Peripherals::steal())
-    }
-
     fn new(_cp: pac::CorePeripherals, dp: pac::Peripherals) -> Board<'a> {
+        let rcc = dp.RCC.constrain();
+        let ccdr_peripheral = unsafe { rcc.steal_peripheral_rec() };
+
         let ccdr: hal::rcc::Ccdr = clocks::configure(dp.PWR.constrain(),
-                                                     dp.RCC.constrain(),
+                                                     rcc,
                                                      &dp.SYSCFG);
 
         let gpioa: gpio::gpioa::Parts = dp.GPIOA.split(ccdr.peripheral.GPIOA);
@@ -99,6 +97,7 @@ impl<'a> Board<'a> {
 
         Self {
             clocks: ccdr.clocks,
+            peripheral: ccdr_peripheral,
             leds: led::Leds {
                 USER: led::LedUser::new(gpioc.pc7)
             },
