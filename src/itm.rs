331970c077bbc838d346ac264bf0520ffc74c2f3
Antoine van Gelder <antoine@flowdsp.io>
2021-02-22 16:07:39 +0200

    Better document itm enabling code

--- src/itm.rs
+++ src/itm.rs
@@ -1,81 +1,76 @@
 #![allow(unused_macros)]
 
 
-// From: https://gist.github.com/diondokter/5740aeb145e123c5b4dac7c7b32e36f6
+#[macro_export]
+macro_rules! loggit {
+    ($($arg:tt)*) => (
+        let itm = unsafe { &mut *cortex_m::peripheral::ITM::ptr() };
+        iprintln!(&mut itm.stim[0], $($arg)*);
+    )
+}
+
+
+/// Adapted from: https://gist.github.com/diondokter/5740aeb145e123c5b4dac7c7b32e36f6
+///
+/// Register addresses:
+///
+///   SWO    base: 0x5C00_3000
+///   SWTF   base: 0x5C00_4000  (SWO funnnel)
+///   DBGMCU base: 0x5C00_1000
+///   ITM    base: 0xE000_0000  (Instrumentation Trace Macrocell)
+///   DCB    base: 0xE000_EDF0  (Debug Control Block)
+
 pub unsafe fn enable_itm(
-    dbgmcu: &stm32h7xx_hal::stm32::DBGMCU,
     dcb: &mut cortex_m::peripheral::DCB,
+    dbgmcu: &stm32h7xx_hal::stm32::DBGMCU,
     itm: &mut cortex_m::peripheral::ITM,
     c_ck: u32,
     swo_frequency: u32,
 ) {
-    // ARMv7-M DEMCR: Set TRCENA. Enables DWT and ITM units
-    //unsafe { *(0xE000_EDFC as *mut u32) |= 1 << 24 };
+    // DCB_DEMCR: Set TRCENA. Enables DWT and ITM units  (DEMCR = Debug Exception and Monitor Control)
+    //*(0xE000_EDFC as *mut u32) |= 1 << 24;
     dcb.enable_trace();
 
-    // Ensure debug blocks are clocked before interacting with them
+    // DBGMCU_CR: Ensure debug blocks are clocked before interacting with them  (page 3350)
+    // *(0x5C00_1004 as *mut u32)
     dbgmcu.cr.modify(|_, w| {
-        w.d1dbgcken()
-            .set_bit()
-            .d3dbgcken()
-            .set_bit()
-            .traceclken()
-            .set_bit()
-            .dbgsleep_d1()
-            .set_bit()
+        w.d3dbgcken().set_bit()   // D3 domain debug clock enable
+         .d1dbgcken().set_bit()   // D1 domain debug clock enable
+         .traceclken().set_bit()  // Enable trace port clock, TRACECLK
+         .dbgsleep_d1().set_bit() // Automatic clock stop/power-down disabled
     });
 
-    // SWO: Unlock
-    *(0x5c00_3fb0 as *mut u32) = 0xC5ACCE55;
-    // SWTF: Unlock
-    *(0x5c00_4fb0 as *mut u32) = 0xC5ACCE55;
+    // SWO_LAR: Unlock SWO (LAR = Lock Access Register)
+    *(0x5c00_3fb0 as *mut u32) = 0xC5AC_CE55;
 
-    // SWO CODR Register: Set SWO speed
-    //*(0x5c00_3010 as *mut _) = crate::system::clock::get_clocks().c_ck().0 / swo_frequency - 1;
-    *(0x5c00_3010 as *mut _) = c_ck / swo_frequency - 1;
+    // SWTF_LAR: Unlock SWTF
+    *(0x5c00_4fb0 as *mut u32) = 0xC5AC_CE55;
 
+    // SWO_CODR Register: Set SWO speed
+    *(0x5c00_3010 as *mut _) = (c_ck / swo_frequency) - 1;
 
-    // SWO SPPR Register:
-    // 1 = Manchester
-    // 2 = NRZ
-    *(0x5c00_30f0 as *mut _) = 2;
+    // SWO_SPPR Register:
+    // 1 = Manchester, 2 = NRZ
+    *(0x5c00_30f0 as *mut _) = 0x2;
 
-    // SWTF Trace Funnel: Enable for CM7
-    *(0x5c00_4000 as *mut u32) |= 1;
+    // SWTF_CTRL Trace Funnel: Enable for CM7
+    *(0x5c00_4000 as *mut u32) |=
+        (0 << 1)               | // ENS1 - Slave port S1 enabled (Cortex-M4)
+        (1 << 0);                // ENS0 - Slave port S0 enabled (Cortex-M7)
 
-    // ITM: Unlock
-    itm.lar.write(0xC5ACCE55);
-    // ITM Trace Enable Register: Enable lower 8 stimulus ports
+    // ITM_LAR: Unlock ITM
+    // *(0xE000_0FB0 as *mut u32) = 0xC5AC_CE55;
+    itm.lar.write(0xC5AC_CE55);
+
+    // ITM_TER: Enable lower 8 stimulus ports (TER = Trace Enable Register)
+    // *(0xE000_0E00 as *mut u32)
     itm.ter[0].write(1);
-    // ITM Trace Control Register: Enable ITM
+
+    // ITM_TCR:  Enable ITM (TCR = Trace Control Register)
+    // *(0xE000_0E80 as *mut u32)
     itm.tcr.write(
-        (0b000001 << 16) | // TraceBusID
-        (1 << 3) | // enable SWO output
-        (1 << 0), // enable the ITM
+        (0b00_0001 << 16) | // TraceBusID
+        (1 << 3)          | // TXENA  - hardware event packet forwarding enable (enable SWO output ?)
+        (1 << 0),           // ITMENA - enable the ITM
     );
-}
-
-
-/*
-#[macro_export]
-macro_rules! loggit {
-    ($($args:expr),*) => {{
-        $(
-            //let itm = &mut cp.ITM;
-            //let itm = unsafe { &mut *cortex_m::peripheral::ITM::ptr() };
-            let itm = &mut cortex_m::Peripherals::steal().ITM;
-            iprintln!(&mut itm.stim[0], "{}", $args);
-        )*
-    }}
-}
- */
-
-#[macro_export]
-macro_rules! loggit {
-    ($($arg:tt)*) => (
-        //let itm = &mut cp.ITM;
-        let itm = unsafe { &mut *cortex_m::peripheral::ITM::ptr() };
-        //let itm = unsafe { &mut cortex_m::Peripherals::steal().ITM };
-        iprintln!(&mut itm.stim[0], $($arg)*);
-    )
 }
