92d315c8e1b1e4d955ddf4f7fa40c989261f83c3
Antoine van Gelder <antoine@flowdsp.io>
2021-03-25 18:29:30 +0200

    Refactor blinky and itm examples to use new crate architecture

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,19 +1,12 @@
 #![no_main]
 #![no_std]
 
+use panic_semihosting as _;
 use cortex_m_rt::entry;
-use cortex_m::iprintln;
 
 use nucleo_h745zi as nucleo;
-
-use nucleo::hal;
-use hal::prelude::*;
-
+use nucleo::hal::prelude::*;
 use nucleo::led::Led;
-use nucleo::loggit;
-
-use log::{debug, error, info};
-mod utilities;
 
 
 #[entry]
@@ -40,45 +33,23 @@ fn main() -> ! {
                                  dp.GPIOJ.split(ccdr.peripheral.GPIOJ),
                                  dp.GPIOK.split(ccdr.peripheral.GPIOK));
 
-
-    // - logger setup ---------------------------------------------------------
-
-    utilities::logger::init();
-
-    let itm = unsafe { &mut *cortex_m::peripheral::ITM::ptr() };
-    iprintln!(&mut itm.stim[0], "Hello itm example!");
-
-    info!("Hello itm example!");
-    debug!("Hello itm example!");
-    error!("Hello itm example!");
-
-
-    // - leds -----------------------------------------------------------------
-
     let mut user_leds = nucleo::led::UserLedsAlt::new(pins.user_leds);
-
 
     // - main loop ------------------------------------------------------------
 
     let one_second = ccdr.clocks.sys_ck().0;
-    let mut counter = 0;
 
     loop {
-        loggit!("{}: one", counter);
         user_leds.ld3.off();
         user_leds.ld1.on();
         cortex_m::asm::delay(one_second);
 
-        loggit!("{}: two", counter);
         user_leds.ld1.off();
         user_leds.ld2.on();
         cortex_m::asm::delay(one_second);
 
-        loggit!("{}: three", counter);
         user_leds.ld2.off();
         user_leds.ld3.on();
         cortex_m::asm::delay(one_second);
-
-        counter += 1;
     }
 }
